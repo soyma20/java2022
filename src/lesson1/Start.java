29e5ceae2d079cfae03ea0ce29541bcc848f57e5
soyma20 <slaviksoyma2014@gmail.com>
2022-06-08 13:49:23 +0300

    java

--- src/lesson1/Start.java
+++ src/lesson1/Start.java
@@ -7,12 +7,15 @@ public class Start {
         System.out.println(post);
         post.setBody("some text");
         System.out.println(post);
+
         String the_title = post.greetings("the title ");
         System.out.println(the_title);
 
         Comment comment = new Comment(1,2,"jdf","dkjfnk","fndklfnldkf");
 
         System.out.println(comment);
+        System.out.println(comment.getBody());
+
 
 
     }
