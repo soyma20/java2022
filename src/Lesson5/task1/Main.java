57059643564cd71acf77567d48c3d881396cd83b
soyma20 <slaviksoyma2014@gmail.com>
2022-06-22 11:37:11 +0300

    java

---
+++ src/Lesson5/task1/Main.java
@@ -0,0 +1,38 @@
+package Lesson5.task1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("word");
+        strings.add("tomato");
+        strings.add("potato");
+        strings.add("pen");
+        strings.add("laptop");
+        strings.add("pc");
+        strings.add("tv");
+        strings.add("socket");
+        strings.add("plug");
+        strings.add("window");
+        strings.add("screen");
+        strings.add("chair");
+        strings.add("spy");
+        strings.add("water");
+        strings.add("earth");
+        strings.add("door");
+        strings.add("dream");
+        strings.add("tree");
+        strings.add("three");
+        strings.add("cow");
+        strings.add("coffee");
+
+        strings.stream().filter(s -> s.length() < 4).sorted(String::compareTo).forEach(System.out::println);;
+
+
+
+
+    }
+}
---
+++ src/Lesson5/task2/Main.java
@@ -0,0 +1,42 @@
+package Lesson5.task2;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] ints = new Integer[20];
+        ints[0] = 0;
+        ints[1] = 123;
+        ints[2] = 21;
+        ints[3] = 32;
+        ints[4] = 453;
+        ints[5] = 2;
+        ints[6] = 1;
+        ints[7] = 8890;
+        ints[8] = 47;
+        ints[9] = -45;
+        ints[10] = -23;
+        ints[11] = 3284;
+        ints[12] = 48;
+        ints[13] = 18;
+        ints[14] = 19;
+        ints[15] = 432;
+        ints[16] = 90;
+        ints[17] = -1;
+        ints[18] = 398;
+        ints[19] = 404;
+
+//        Arrays.stream(ints).sorted((o1, o2) -> o1 - o2).forEach(System.out::println);
+//        Arrays.stream(ints).filter(integer -> integer % 3 == 0).forEach(System.out::println);
+//        Arrays.stream(ints).filter(integer -> integer % 10 == 0).forEach(System.out::println);
+//        Arrays.stream(ints).map(integer -> integer*3).forEach(System.out::println);
+
+        Arrays.stream(ints)
+                .sorted((o1, o2) -> o1 - o2)
+                .filter(integer -> integer % 3 == 0)
+                .filter(integer -> integer % 10 == 0)
+                .map(integer -> integer * 3)
+                .forEach(System.out::println);
+    }
+}
